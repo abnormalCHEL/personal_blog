817ddbfa4510b7cada5b8d955b88e5d22d2072be
unknown <jacowest36@gmail.com>
2024-10-15 14:09:50 +0300

    changes

--- core/urls.py
+++ core/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('about/', about),
     path('portfolio/' ,Paartfolia),
     path('blog/<int:pk>', blog_detail, name='blog_detail')
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
